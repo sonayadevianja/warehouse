744c1694a96666091c94f89e83412068d611f63a
Alfira Santa Praja <alfirasanta20@gmail.com>
2023-07-15 18:55:09 +0700

    database ges

---
+++ database/migrations/2023_07_12_152911_create_barang_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_barang');
+            $table->date('tanggal_produksi');
+            $table->foreignId('jenis_id')->constrained();
+            $table->date('tanggal_keluar');
+            $table->date('tanggal_masuk');
+            $table->integer('jumlah');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang');
+    }
+};
