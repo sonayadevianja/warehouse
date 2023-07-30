cf1b141c07ebbcfad52be2afcec14d3463c7f8b5
Alfira Santa Praja <alfirasanta20@gmail.com>
2023-07-30 20:11:29 +0700

    stok tapi masih belum bisa nambah

--- resources/views/barang/actions.blade.php
+++ resources/views/barang/actions.blade.php
@@ -1,6 +1,7 @@
 <div class="d-flex">
     <a href="#" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
     <a href="#" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
+    <a href="/barang/barangmasuk/{{ $barang->id }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-plus-circle-fill"></i></a>
     <div>
         <form action="{{ route('barang.destroy', ['barang' => $barang->id]) }}" method="POST" data-name="{{ $barang->nama_barang.' '.$barang->tanggal_produksi }}">
             @csrf
